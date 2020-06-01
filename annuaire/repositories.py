0151cd2f9be3c10f30796819eb17feb14a008976
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-06-01 16:32:23 +0200

    start repository

--- annuaire/repositories.py
+++ annuaire/repositories.py
@@ -1,26 +1,28 @@
+
 class PersonnRepository:
+    """[summary]."""
+
     def __init__(self, db):
+        """[summary].
+
+        Arguments:
+            db {[type]} -- [description]
+        """
         self.db = db
 
     def get_all(self):
-        pass
+        """[summary]."""
+        rows = self.db.query("SELECT * FROM personne ")
+        return rows
 
-    def create(self, name, forename, phone_number, email, adresse):
-        pass
+    def create(self, data):
+        """[summary]."""
+        rows = self.db.query(
+            """INSERT INTO personne (name, forname,email) VALUES (:name, :forname,:email)""",
+            **data
+        )
 
-    def save(self, personne):
-        pass
-
-    def remove(self, personne):
-        pass
-
-
-class AdresseRepository:
-    def __init__(self, db):
-        self.db = db
-
-    def create(self, street, country, postal_code, number, persons):
-        pass
-
-    def save(self, adresse):
-        pass
+    def select_by_field_name(self, field):
+        """[summary]."""
+        rows = self.db.query("""SELECT (field) FROM personne VALUES (%s)""")
+        return
