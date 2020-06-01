9fa3337829d693e96331a0b843d4aa2ff1b91048
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-06-01 21:17:02 +0200

    test research api

--- api/open_food_fact.py
+++ api/open_food_fact.py
@@ -2,18 +2,37 @@ import requests
 
 payload = {"search_terms": "Nestle", "json": 1}
 
+CATEGORIES = ["Plats préparés", "Fromages", "Desserts", "Viandes"]
+for category in CATEGORIES:
+    config = {
+        "action": "process",
+        "tagtype_0": "categories",
+        "tag_0": category,
+        "tag_contains_0": "contains",
+        "page_size": 30,
+        "json": 1,
+    }
 
-def search_api(url, payload):
+
+def search_api(url):
     liste = []
-    res = requests.get(url, params=payload)
-    results = res.json()
-    product = results["products"]
+    for category in CATEGORIES:
+        config = {
+            "action": "process",
+            "tagtype_0": "categories",
+            "tag_0": category,
+            "tag_contains_0": "contains",
+            "page_size": 10,
+            "json": 1,
+        }
+        res = requests.get(url, params=config)
+        results = res.json()
+        product = results["products"]
     for products in product:
         liste.append(products["product_name"])
     return liste
 
 
 if __name__ == "__main__":
-    payload = {"search_terms": "Nestle", "json": 1}
-    a = search_api("https://fr.openfoodfacts.org/cgi/search.pl?", payload)
+    a = search_api("https://fr.openfoodfacts.org/cgi/search.pl?")
     print(a)
