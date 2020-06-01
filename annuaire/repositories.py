1fc4befb4224feaeeb1d11032ff388dacd1b91ba
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-06-01 20:35:37 +0200

    modify argumment methode create

--- annuaire/repositories.py
+++ annuaire/repositories.py
@@ -15,11 +15,11 @@ class PersonnRepository:
         rows = self.db.query("SELECT * FROM personne ")
         return rows
 
-    def create(self, data):
+    def create(self, name, forname, email):
         """[summary]."""
         rows = self.db.query(
             """INSERT INTO personne (name, forname,email) VALUES (:name, :forname,:email)""",
-            **data
+            name=name, forname=forname, email=email
         )
 
     def select_by_field_name(self, field):
