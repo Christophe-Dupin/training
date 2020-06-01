871cfc544fe9f7c06f5d6f9674535f33999a571d
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-06-01 17:39:05 +0200

    first script api openfoodfact

---
+++ api/open_food_fact.py
@@ -0,0 +1,21 @@
+import requests
+
+payload = {"search_terms": "Nestle", "json": 1}
+
+
+def search_api(url, payload):
+    liste = []
+    res = requests.get(url, params=payload)
+    results = res.json()
+    product = results["products"]
+    for products in product:
+        liste.append(products["product_name"])
+    return liste
+
+
+"https://fr.openfoodfacts.org/cgi/search.pl?"
+
+if __name__ == "__main__":
+    payload = {"search_terms": "Nestle", "json": 1}
+    a = search_api("https://fr.openfoodfacts.org/cgi/search.pl?", payload)
+    print(a)
