421dd41b565927f518ebf08e248635c21fa39b44
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2020-06-01 17:51:49 +0200

    remove unused link

--- api/open_food_fact.py
+++ api/open_food_fact.py
@@ -13,8 +13,6 @@ def search_api(url, payload):
     return liste
 
 
-"https://fr.openfoodfacts.org/cgi/search.pl?"
-
 if __name__ == "__main__":
     payload = {"search_terms": "Nestle", "json": 1}
     a = search_api("https://fr.openfoodfacts.org/cgi/search.pl?", payload)
